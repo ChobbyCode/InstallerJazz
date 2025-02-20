2d69d537a8ebd9b09fb104b33cfda96a66597ba3
ChobbyCode <jacobhadfield99@gmail.com>
2025-02-20 10:53:45 +0000

    Installer

---
+++ Installer/Installer/AppInstaller.cs
@@ -0,0 +1,11 @@
+﻿using InstallerJazz.Models;
+using InstallerJazz.FileUtils.SourceDownloader;
+
+namespace InstallerJazz.Installer {
+    public class AppInstaller {
+        public AppInstaller(InstallArguments InstallArguments) {
+            SourceDownloader sourceDownloader = new SourceDownloader();
+            sourceDownloader.DownloadAndCopy(InstallArguments.SourceURL, InstallArguments.TargetLocation);
+        }
+    }
+}
---
+++ InstallerJazz/Models/InstallArguments.cs
@@ -0,0 +1,23 @@
+﻿
+namespace InstallerJazz.Models {
+    public class InstallArguments {
+        // Generic Settings
+        public string WindowName { get; set; }
+        public int InstallerVersion { get; set; }
+        public int AppVersion { get; set; }
+        public bool RunInBackground { get; set; }
+        // Where To Download App
+        public string SourceURL { get; set; }
+        public string VersionInfoURL { get; set; }
+        public bool IsZipBall { get; set; }
+        public bool GithubMode { get; set; }
+        // How To Deal With Downloaded Files
+        public bool EnableInstallFeature { get; set; }
+        public bool EnableUpdateFeature { get; set; }
+        public bool EnableUninstallFeature { get; set; }
+        // User Interaction Settings
+        public string TargetLocation { get; set; }
+        public bool AllowUsersToChooseInstallDrive { get; set; }
+        public bool AllowUsersToChooseFullInstallPath { get; set; }
+    }
+}
