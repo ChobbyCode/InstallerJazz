ea2d1d493f38492af9e44c46d820f5a0a3d356b9
ChobbyCode <jacobhadfield99@gmail.com>
2025-02-20 15:10:38 +0000

    Loading Dialogue

--- Installer/Installer/AppInstaller.cs
+++ Installer/Installer/AppInstaller.cs
@@ -3,9 +3,33 @@ using InstallerJazz.FileUtils.SourceDownloader;
 
 namespace InstallerJazz.Installer {
     public class AppInstaller {
+
+        bool load = true;
+
         public AppInstaller(InstallArguments InstallArguments) {
             SourceDownloader sourceDownloader = new SourceDownloader();
+
+            Thread Loading = new Thread(new ThreadStart(RenderLoadingIcon));
+            Loading.Start();
+
             sourceDownloader.DownloadAndCopy(InstallArguments.SourceURL, InstallArguments.TargetLocation);
+
+            load = false;
+            Thread.Sleep(300);
+        }
+
+
+        private void RenderLoadingIcon() {
+            var loaderChars = new[] { '/', '-', '\\', '|' };
+            var a = 0;
+
+            while (load) {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(loaderChars[a++]);
+                a = a == loaderChars.Length ? 0 : a;
+                Thread.Sleep(300);
+            }
+            Console.SetCursorPosition(0, Console.CursorTop);
         }
     }
 }
